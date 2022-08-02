2f378b247884892d44f2f098ab6867578351b423
eguir <eguir@advestis.com>
2022-08-02 16:07:43 +0200

    add method of information of the series, add method to detect outliers in relation to the minimum and maximum finish all pytest

--- addetect/detector.py
+++ addetect/detector.py
@@ -1,10 +1,12 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from scipy import stats
+# from statsmodels.tsa.stattools import adfuller
+# from statsmodels.tsa.arima.model import ARIMA
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import IsolationForest
 
 import logging
 
@@ -16,25 +18,11 @@ class Detector:
         "standard_deviation",
         "iqr",
         "zscore",
-        "gaussian_mixture_model",
-        "extreme_value_analysis",
-        "local_outlier_factor",
-        "connectivity_based_outlier_detection",
-        "angular_based_outlier_detection",
-        "dbscan_clustering",
-        "kmeans_clustering",
-        "knearest_neighbor",
-        "mahalanobis_distance",
-        "isolation_forest",
-        "support_vector_machine",
-        "minimum",
-        "maximum",
     ]
 
-    # TODO : Verif the type of the index @eguir HINT (pcotte): use 'adtypingdecorator'
     def __init__(
             self,
-            serie: Optional[pd.Series] = None,
+            series: Optional[pd.Series] = None,
             detect_methods: Optional[List[str]] = None,
     ):
         """
@@ -42,7 +30,7 @@ class Detector:
 
         Parameters
         ----------
-        serie : pd.Series
+        series : pd.Series
             If None, will have to be specified later by setting the instance's 'series' attribute.
         detect_methods : List[str]
             If None, will default to 'standard_deviation'.
@@ -54,9 +42,9 @@ class Detector:
         ValueError
             If any of the detection methods is unknown by the class.
         """
-        if not isinstance(serie, pd.Series):
-            raise TypeError(f"The type of the series must be pd.Series and not {type(serie)}")
-        self._serie = serie
+        if not isinstance(series, pd.Series):
+            raise TypeError(f"The type of the series must be pd.Series and not {type(series)}")
+        self._serie = series
         if detect_methods is not None:
             for d_meth in detect_methods:
                 if d_meth not in self.DETEC_METHOD:
@@ -119,11 +107,11 @@ class Detector:
             True if p-value < alpha_lever
             False else
         """
-        serie = self.serie.dropna()
-        df_res = pd.DataFrame(serie)
-        ks = stats.ks_1samp(serie, stats.norm.cdf)
-        df_res.loc["p-value", serie.name] = ks.pvalue
-        if df_res.loc["p-value", serie.name] < alpha_level:
+        series = self.serie.dropna()
+        df_res = pd.DataFrame(series)
+        ks = stats.ks_1samp(series, stats.norm.cdf)
+        df_res.loc["p-value", series.name] = ks.pvalue
+        if df_res.loc["p-value", series.name] < alpha_level:
             return True
         else:
             return False
@@ -137,11 +125,11 @@ class Detector:
         pd.Series
             outliers is a series containing date and value of outliers.
         """
-        serie = self.serie.dropna()
-        res = pd.DataFrame(serie)
-        res["z-score"] = serie.apply(lambda x: self.get_zscore(x))
+        series = self.serie.dropna()
+        res = pd.DataFrame(series)
+        res["z-score"] = series.apply(lambda x: self.get_zscore(x))
         res = res[res["z-score"] > 3]
-        outliers = res[serie.name]
+        outliers = res[series.name]
         return outliers
 
     def _iqr(self) -> pd.Series:
@@ -154,14 +142,14 @@ class Detector:
         pd.Series
             outliers is a series containing date and value of outliers
         """
-        serie = self.serie.dropna()
-        quantile_1 = np.quantile(serie, 0.25)
-        quantile_3 = np.quantile(serie, 0.75)
+        series = self.serie.dropna()
+        quantile_1 = np.quantile(series, 0.25)
+        quantile_3 = np.quantile(series, 0.75)
         iqr = quantile_3 - quantile_1
         lower_bound = quantile_1 - 1.5 * iqr
         upper_bound = quantile_3 + 1.5 * iqr
 
-        outliers = serie[(serie < lower_bound) | (serie > upper_bound)]
+        outliers = series[(series < lower_bound) | (series > upper_bound)]
         return outliers
 
     def _standard_deviation(self) -> pd.Series:
@@ -177,41 +165,115 @@ class Detector:
             ValueError
         """
         if self._verif_norm() is False:
-            # TODO maybe change the type of error @eguir
             raise ValueError("The series does not follow a normal law, so we cannot use this method")
-        serie = self.serie.dropna()
-        mean, std = np.mean(serie), np.std(serie)
+        series = self.serie.dropna()
+        mean, std = np.mean(series), np.std(series)
         cut_off = std * 3
         lower, upper = mean - cut_off, mean + cut_off
-        outliers = serie[(serie < lower) | (serie > upper)]
+        outliers = series[(series < lower) | (series > upper)]
 
         return outliers
 
-    def _isolation_forest(self, outlier_fraction=.01) -> pd.Series:
+    def _outlier_by_max(self, maximum) -> pd.Series:
         """
-        The isolation forest attempts to separate each point from the data. In the case of 2D,
-        it randomly creates a line
-        and tries to isolate a point. In this case, an abnormal point can be separated in a few steps, while normal
-        points that are closer together may take many more steps to separate.
+        This method find the outliers higher than the maximum
+
+        Parameters
+        ----------
+        maximum : int
+            The maximum value
 
         Returns
         -------
         pd.Series
-            Series containing the outliers
+            The serie with the outliers
         """
-        scaler = StandardScaler()
-        serie = self.serie.dropna()
-        df_res = pd.DataFrame(serie)
-        np_scaled = scaler.fit_transform(serie.values.reshape(-1, 1))
-        data = pd.DataFrame(np_scaled)
-        model = IsolationForest(contamination=outlier_fraction)
-        model.fit(data)
-        df_res['anomaly'] = model.predict(data)
-        a = df_res.loc[df_res['anomaly'] == -1, [serie.name]]  # anomaly
-        print(a)
-        return a
+        series = self.serie.dropna()
+        df_res = pd.DataFrame(series)
+        series = series[df_res[series.name] > maximum]
+        return series
 
-    # TODO pytest @eguir
+    def _outlier_by_min(self, minimum) -> pd.Series:
+        """
+        This method find the outliers higher than the minimum
+
+        Parameters
+        ----------
+        minimum : int
+            The minimum value
+
+        Returns
+        -------
+        pd.Series
+            The serie with the outliers
+        """
+        series = self.serie.dropna()
+        df_res = pd.DataFrame(series)
+        series = series[df_res[series.name] < minimum]
+        return series
+
+    # def _isolation_forest(self, outlier_fraction=.01) -> pd.Series:
+    #     """
+    #     The isolation forest attempts to separate each point from the data. In the case of 2D,
+    #     it randomly creates a line
+    #     and tries to isolate a point. In this case, an abnormal point can be separated in a few steps, while normal
+    #     points that are closer together may take many more steps to separate.
+    #
+    #     Returns
+    #     -------
+    #     pd.Series
+    #         Series containing the outliers
+    #     """
+    #     scaler = StandardScaler()
+    #     serie = self.serie.dropna()
+    #     df_res = pd.DataFrame(serie)
+    #     np_scaled = scaler.fit_transform(serie.values.reshape(-1, 1))
+    #     data = pd.DataFrame(np_scaled)
+    #     model = IsolationForest(contamination=outlier_fraction)
+    #     model.fit(data)
+    #     df_res['anomaly'] = model.predict(data)
+    #     a = df_res.loc[df_res['anomaly'] == -1, [serie.name]]  # anomaly
+    #     print(a)
+    #     return a
+
+    # # TODO : not finish to improve
+    # def _arima(self):
+    #     """
+    #     Find outliers with arima method.
+    #
+    #     Returns
+    #     -------
+    #     pd.Series
+    #         Outliers
+    #     """
+    #
+    #     def test_stationarity(ts_data, signif=0.05):
+    #         adf_test = adfuller(ts_data, autolag='AIC')
+    #         p_value = adf_test[1]
+    #         if p_value <= signif:
+    #             test_result = "Stationary"
+    #         else:
+    #             test_result = "Non-Stationary"
+    #         return test_result
+    #
+    #     def find_anomalies(squared_errors):
+    #         threshold = np.mean(squared_errors) + np.std(squared_errors)
+    #         predictions = (squared_errors >= threshold).astype(int)
+    #         return predictions, threshold
+    #
+    #     if test_stationarity(self.serie.dropna()) != "Stationary":
+    #         raise TypeError("Your series is not stationary, so this method cannot be applied")
+    #
+    #     serie = self.serie.dropna()
+    #     arma = ARIMA(serie, order=(0, 0, 0))
+    #     arma_fit = arma.fit()
+    #     squared_errors = arma_fit.resid ** 2
+    #     predictions, threshold = find_anomalies(squared_errors)
+    #     data = pd.DataFrame(serie)
+    #     data['predictions'] = predictions
+    #     data = data.loc[data["predictions"] == 1, self.serie.name]
+    #     return data
+
     def _detect_outliers(self) -> List[list]:
         """
         This method detects outliers based on self._detetct_method.
@@ -223,10 +285,9 @@ class Detector:
         """
         output = []
         for m in self.detect_method:
-            output.append(getattr(self, f"_{m}"))
+            output.append(getattr(self, f"_{m}")())
         return output
 
-    # TODO pytest
     def _first_date(self):
         """
         This method finds the first non-null date.
@@ -238,7 +299,6 @@ class Detector:
         """
         return self.serie.first_valid_index()
 
-    # TODO pytest
     def _last_date(self):
         """
         This method finds the last non-null date.
@@ -251,7 +311,6 @@ class Detector:
         """
         return self.serie.last_valid_index()
 
-    # TODO pytest
     def _count_date(self):
         """
         Count the number of dates from the first to the last non-null index
@@ -264,19 +323,17 @@ class Detector:
 
         return len(self.serie.loc[self._first_date():self._last_date()])
 
-    # TODO pytest
     def _serie_between_first_and_last_index(self):
         """
-        Get the serie between the first and last date non-null
+        Get the serie between the first and last date non-null.
 
         Returns
         -------
         pd.Series
-
+            The serie between the first and last non-null date.
         """
         return self.serie.loc[self._first_date():self._last_date()]
 
-    # TODO pytest
     def _count_nan_between_index(self):
         """
         Count the number of nan between the first and last index
@@ -288,7 +345,6 @@ class Detector:
         """
         return self.serie.loc[self._first_date():self._last_date()].isna().sum()
 
-    # TODO pytest
     def _not_jump_date(self, freq="B"):
         """
         Check that no dates are missing according to the frequency
@@ -303,8 +359,7 @@ class Detector:
         return len(pd.date_range(start=self._first_date(), end=self._last_date(), freq=freq)) == len(
             self._serie_between_first_and_last_index())
 
-    # TODO pytest
-    def verif_not_duplicate_index(self):
+    def _verif_not_duplicate_index(self):
         """
         Check that there are no duplicate indexes.
 
@@ -316,7 +371,6 @@ class Detector:
         """
         return self.serie.index.duplicated().sum() == 0
 
-    # TODO pytest
     def _get_minimum(self) -> float:
         """
         Get the minimum of the serie.
@@ -328,7 +382,6 @@ class Detector:
         """
         return min(self.serie)
 
-    # TODO pytest
     def _get_maximum(self) -> List:
         """
         Get the maximum of the serie.
@@ -340,7 +393,6 @@ class Detector:
         """
         return max(self.serie)
 
-    # TODO pytest
     def _get_standard_deviation(self) -> float:
         """
         Get the standard deviation of the serie.
@@ -352,7 +404,6 @@ class Detector:
         """
         return float(np.std(self.serie))
 
-    # TODO pytest
     def _get_mean(self) -> float:
         """
         Get the standard deviation of the serie.
@@ -364,8 +415,7 @@ class Detector:
         """
         return float(np.mean(self.serie))
 
-    # TODO pytest
-    def _get_first_quantile(self):
+    def _get_first_quantile(self) -> float:
         """
         Get the first quantile of the serie.
 
@@ -377,21 +427,19 @@ class Detector:
         """
         return np.quantile(self.serie.dropna(), 0.25)
 
-    # TODO pytest
-    def _get_last_quantile(self):
+    def _get_last_quantile(self) -> float:
         """
         Get the first quantile of the serie.
 
         Returns
         -------
         float
-            The first quantile
+            The last quantile
 
         """
         return np.quantile(self.serie.dropna(), 0.75)
 
-    # TODO pytest
-    def _get_median(self):
+    def _get_median(self) -> float:
         """
         Get the median of the serie.
 
@@ -400,58 +448,89 @@ class Detector:
         float
             median of serie
         """
-        return np.median(self.serie.dropna())
+        return float(np.median(self.serie.dropna()))
 
-    # TODO pytest
-    def _verif_type_of_serie(self, type):
+    def _verif_type_of_serie(self, kind) -> bool:
         """
         Check that the data type of our series matches the one in parameter
 
-        Returns
+        Return
         -------
-        float
-            median of serie
+        bool
+            true if type corresponding
+            false else
         """
 
-        if type is None:
+        if kind is None:
             raise ValueError("The type cannot be null")
-        try:
-            return isinstance(self.serie.dtypes, type)
-        except TypeError:
-            raise TypeError(f"The type {type} does not exist")
+        return self.serie.dtypes is kind
 
-    # TODO pytest
     def _variation_between_date(self) -> pd.Series:
         """
+        Get the serie with the pct change.
 
-        Returns
+        Return
         -------
         pd.Series
             The serie with the pct change for each value
         """
-        return self.serie.dropna().pct_change()
+        return self.serie.pct_change()
 
-    # TODO pytest
     def _max_variation(self) -> float:
         """
+        Get the max variation after pct_change
 
-        Returns
+        Return
         -------
-
+        int
+            max of pct_change
         """
 
         return max(self._variation_between_date().dropna())
 
-    # TODO pytest
     def _min_variation(self) -> float:
         """
+        Get the min variation after pct_change
 
-        Returns
+        Return
         -------
-
+        int
+            min of pct_change
         """
 
         return min(self._variation_between_date().dropna())
+
+    def _nb_flat(self) -> Tuple[int, pd.DataFrame]:
+        """
+        Calculate the number of times there are flat values.
+
+        Return
+        -------
+        int
+            The number of flat
+        pd.Dataframe
+            The dataframe with the flat
+        """
+        df_change = self.serie.dropna().pct_change()
+        df_change.columns = [self.serie.name]
+        df_change = pd.DataFrame(df_change)
+        df_change['flat'] = (df_change[self.serie.name] != df_change[self.serie.name].shift(1)).cumsum()
+        df_change = df_change[df_change[self.serie.name] == 0].groupby('flat', as_index=False).count()
+        return len(df_change[self.serie.name]), df_change[self.serie.name]
+
+    def _max_len_flat(self):
+        """
+        Compute the longest flat
+
+        Return
+        -------
+        int
+            The longest flat
+        """
+        if (self._nb_flat()[1]).empty:
+            return 0
+        else:
+            return max(self._nb_flat()[1]) + 1
 
     def _plot_with_outliers(self, outliers: pd.Series, show: bool = False):
         """
@@ -461,13 +540,93 @@ class Detector:
         outliers : pd.Serie
             The serie of outliers
         """
-        serie = self.serie.dropna()
+        series = self.serie.dropna()
         plt.figure(figsize=(20, 10))
         plt.xlabel("Date")
-        plt.ylabel(serie.name)
-        plt.plot(pd.DatetimeIndex(serie.index), serie.values)
+        plt.ylabel(series.name)
+        plt.plot(pd.DatetimeIndex(series.index), series.values)
         plt.scatter(pd.DatetimeIndex(outliers.index), outliers.values, c="red")
         plt.savefig("plot/plot")
         if show:
             plt.show()
 
+
+df = pd.read_csv("/home/eguin/PycharmProjects/addetect/notebook/gm_matrix.csv", index_col=0)
+# print(df.iloc[::, :1])
+
+serie = df
+serie2 = df["SWAP_INFLA_EUR_1Y_LAST_Z250D"]
+serie3 = pd.Series([-9, -9, -9, -9, 10, -9, -9, 10, 11, 1, 1, 1, 10, 1, 1, 12, 12, 1, 1, -9], name="aa")
+serie13 = pd.Series([4, -9, 7, 2, 1, 12, 7, 2, 3, 9, -2, 6], index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+# print(type(serie2.index[0]))
+# serie2.hist()
+# plt.show()
+# #
+c1 = Detector(serie3)
+print(((c1._outlier_by_min(2))))
+
+# print(c1.nb_flat())
+
+# print(c1._isolation_forest())
+# out = c1._isolation_forest()
+# c1._plot_with_outliers(out, True)
+# c1.drop_outliers(out)
+# c1._plot_with_outliers(out, True)
+# # print(c1._iqr())
+# c2 = Detector(serie2)
+# out2 = c2._zscore()
+# c2._plot_with_outliers(out2, True)
+
+# print(c1.drop_outliers(out2))
+
+# print(len(out))
+#
+# # print("TEST", len(c1._standard_deviation()), "TEST14")
+#
+# res = c1._standard_deviation()
+# print("SANS REPLACE", res)
+# res2 = c1.replace_by_post_value(res)
+# print("AVEC REPLACE", res2)
+# #
+# c2 = Detector(res2)
+# print(c2._zscore())
+
+"---------------"
+#
+# c3 = Detector(serie=pd.Series(
+#     [10, 12, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 21, 24, 35, 39, 21, 45, 159, 180],
+#     index=pd.date_range(start="2022-01-01", end="2022-01-25"), name="value"))
+#
+# print(c3.verif_norm())
+# length = 3000
+# data = np.random.gamma(1, 1, 5)
+# ser = pd.Series(data, index=pd.date_range(start="2022-02-12", end="2022-02-16"), name="value")
+# ser.hist()
+# plt.show()
+# c4 = Detector(ser)
+# print(c4._verif_norm())
+# print(pd.Series(data, index=pd.date_range(start="2013-12-01", end="2022-02-16"), name="value"))
+# print(type(data))
+
+# print(res)
+# print(c1._z_score(), "------", len(c1._z_score()))
+# res = c1._z_score().index
+#
+# serie2.drop(res, inplace=True)
+# serie2.plot()
+# plt.show()
+
+# df = pd.read_csv("/home/eguin/PycharmProjects/addetect/notebook/gm_matrix.csv", index_col=0)
+# print(df.iloc[::, :1])
+
+# serie = df
+# serie2 = df["BOVES_DY 12M_DSTD20_100"].dropna()
+# serie2 = serie2.replace(0, np.nan)
+# serie2.fillna(method="ffill", inplace=True)
+# # serie2.replace(0, np.nan, inplace=True)
+# serie2.plot()
+# plt.show()
+# #
+# c1 = Detector(serie2)
+#
+# print(c1._z_score(), "------", len(c1._z_score()))
